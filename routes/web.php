eb5156985da5ec8f958392d34dd3c430ddd0c3e3
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2026-03-02 20:44:12 +0700

    Commit 2 Josua
    Done With The New and Improve Route

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,27 @@ Route::middleware('auth')->group(function () {
 // ... kode rute lainnya di atas ...
 
 Route::get('/absensi-siswa', function () {
-    // Tambahkan jalur foldernya: app/kepala-sekolah/
+
     return Inertia::render('app/kepala-sekolah/AbsensiSiswa');
 })->name('absensi.siswa');
 
 Route::get('/absensi-guru', function () {
-    // Tambahkan jalur foldernya: app/kepala-sekolah/
+
     return Inertia::render('app/kepala-sekolah/AbsensiGuru');
 })->name('absensi.guru');
 
-// Jangan lupa sekalian tambahkan untuk Perangkat Ajar ya!
+
 Route::get('/perangkat-ajar', function () {
     return Inertia::render('app/kepala-sekolah/PerangkatAjar');
 })->name('perangkat.ajar');
+
+Route::get('/evaluasi-kinerja', function () {
+    return Inertia::render('app/kepala-sekolah/EvaluasiKinerjaGuru');
+})->name('evaluasi.kinerja');
+
+
+Route::get('/pkl', function () {
+    return Inertia::render('app/kepala-sekolah/PKL');
+})->name('pkl');
 
 require __DIR__.'/auth.php';
